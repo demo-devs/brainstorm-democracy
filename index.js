601de37c63fbf59459fca7db4d3bc6be3a05f88a
mel-booth <mel.m.booth@gmail.com>
2016-11-25 12:29:41 +1300

    update so ideas render

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ import {createStore} from 'redux'
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
-// const reducer = require('./reducer')
+const reducer = require('./reducer')
 
 
 const initialState = {
-
   ideas: [
     { id: 1, desc: 'Make a brainstorm democracy app' },
     { id: 2, desc: 'Get a massage'}
@@ -17,8 +16,7 @@ const initialState = {
   ]
 }
 
-
-// const store = createStore(reducer, initialState)
+const store = createStore(reducer, initialState)
 
 const Input = (props) => {
   return (
@@ -26,25 +24,34 @@ const Input = (props) => {
     <input type='text' placeholder='Type your idea here' />
     <input type='submit' value='Submit' />
   </div>
-)
+)}
+
+
+const Idea = (props) => {
+  return <div>
+    <p>IDEA</p>
+      <h3>{props.desc}</h3>
+      </div>
 }
-const Idea = (props => {
-  return <div> <h3>{props.desc}</h3> </div> })
 
 const Ideas = (props) => {
-   return <div>
-    {props.ideas.map((idea) => {
-      return <Idea desc={idea.desc} />
-    })}
-  </div>
+  return (
+    <div>
+      {props.ideas.map((idea)=> {
+        return <Idea desc={idea.desc} />
+      })}
+    </div>)
 }
 
 const main = document.querySelector('main')
-render(<Input />, main)
 
-// store.subscribe(() => {
-//   const state = store.getState()
-//   render(<Input dispatch={store.dispatch} />, main)
-// })
-//
-// store.dispatch({type: 'INIT'})
+store.subscribe(() => {
+  const state = store.getState()
+  render(
+  <div>
+    <Input  />
+    <Ideas ideas={state.ideas}/>
+  </div>, main)
+})
+
+store.dispatch({type: 'INIT'})
